8e9bf26db3d877634814cc93dac555bf0f87dffa
Jangwoo Park <p.jangwoo@gmail.com>
2023-07-19 13:19:42 +0900

    7월 19일 - 점심

--- src/main/java/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -1,5 +1,8 @@
 package com.mysite.sbb;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
@@ -17,6 +20,27 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
 		//findAll()   : select
 		//save() 	  : insert , update
 		//delete()    : delete
-		
+	
+	//JPA 메소드를 사용한 테이블 검색 : 기본적으로 2개의 메소드는 자동으로 등록 되어 있음. 
+			// 그외는 등록 해서 사용 해야 한다. 
+		//findAll()    :  select * from question    <== 모든 레코드를 출력 
+		//findById(1)     : select * from question where id = 1   <== id를 기준으로 레코드 1개 
+	
+	//검색된 레코드가 1개일때는 Optional 에 저장 
+	//검색된 레코드가 여러개일때는 List 에 저장 
+	//Select * from question where subject = ' '  
+	List<Question> findBySubject(String subject); 
+	
+	//Select * from question where content = '' 
+	List<Question> findByContent(String content); 
+	
+	//특정 컬럼의 값을 검색 : Like 검색   <=== 레코드가 여러개가 검색 : List 
+	//Select * from question where subject like '%?%'; 
+	List<Question> findBySubjectLike(String subject); 
+	
+	//select * from question where content like '%?%'
+	List<Question> findByContentLike(String content); 
+	
+	
 
 }
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +25,8 @@ class SbbApplicationTests {
 		q1.setContent("오늘의 날씨는 어떠한지 궁금합니다.");
 		q1.setCreateDate(LocalDateTime.now());
 		
-		qr.save(q1); 
+		qr.save(q1);
+	
 		
 		Question q2 = new Question(); 
 		
@@ -63,16 +65,72 @@ class SbbApplicationTests {
 			//findAll()  : select * from question;
 		
 			//assertEquals (기대치, 값) ; 
-		assertEquals(4, all.size()); 	
+		assertEquals(4, all.size()); 
+		
 		
 	}
-	@Test
+	//@Test
 	void subjectTest() {		
 		List<Question> all = qr.findAll(); 		
 		Question q = all.get(1); 
+
+		assertEquals("스프링 부트 질문 ", q.getSubject()); 		
+	}
+	
+	//@Test
+	void idSearch () {
 		
-		assertEquals("스프링 부트 질문 ", q.getSubject()); 
+		//Optional null을 쉽게 처리해주는 객체 
+		// qr.findById(1)  <=====> select * from question where id = 1 ; 
+		Optional<Question> o = qr.findById(1); 
+		
+		// isPresent() : Optional 에 저장된 객체가 null 이아닐때 
+		if (o.isPresent()) {
+			Question q = o.get() ;     // Optional 에 저장된 객체를 끄집어 내서 q 변수에 할당. 
 			
+			// assertEquals(1, q.getId()) ; 
+			assertEquals("오늘의 날씨는? ", q.getSubject()); 
+		}
+		
+	}
+	//@Test	// 제목으로 검색 Test 
+	void searchSubject() {
+		List<Question> all = qr.findBySubject("오늘의 날씨는? "); 
+		
+		Question q = all.get(0); 
+		
+		assertEquals("오늘의 날씨는? ", q.getSubject()); 
+		assertEquals(1, all.size());
+		
+	}
+	//@Test
+	void serchContent() {
+		List<Question> all = qr.findByContent("오늘의 날씨는 어떠한지 궁금합니다."); 
+		
+		Question q = all.get(0); 
+		
+		assertEquals("오늘의 날씨는 어떠한지 궁금합니다.", q.getContent()); 	
+	}
+	//@Test
+	void searchSubjectLike() {
+		//select * from question where subject like '%날씨%' ; 
+		List<Question> all = qr.findBySubjectLike("%날씨%"); 
+		
+		Question q = all.get(1); 
+		
+		assertEquals(2 , all.size()); 
+		assertEquals(4, q.getId()); 
+		
+	}
+	@Test
+	void serchContentLike() {
+		List <Question> all = qr.findByContentLike("%궁금%"); 
+		
+		Question q = all.get(3); 
+		
+		assertEquals(4, all.size());
+		assertEquals(4, q.getId()); 
+		
 	}
 	
 	
